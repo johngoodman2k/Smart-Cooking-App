4f14ea0319ac8736cfe0b36cab3fd26bec6f9334
johngoodman2k <77042283+johngoodman2k@users.noreply.github.com>
2021-05-09 22:25:03 +0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const showMoreBtn = document.querySelector('.show-more-btn');
-const text = document.querySelector('.text');
-
-showMoreBtn.addEventListener('click', (e) => {
-    text.classList.toggle('read-more');
-})
